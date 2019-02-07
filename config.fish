76216f2265870ae322291f2e49e3482416d9e177
ggarbern <akira080@gmail.com>
2019-02-06 16:07:16 -0800

    make vscode default editor

--- config.fish
+++ config.fish
@@ -9,9 +9,11 @@ end
 
 set -U fish_user_paths $HOME/.cargo/bin $HOME/.local/bin $HOME/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src $fish_user_paths
 set -gx FZF_DEFAULT_COMMAND  'rg --files --no-ignore-vcs --hidden --glob "!.git/*"'
-set -g -x EDITOR nvim
+set -g -x EDITOR code
 set -g -x CLICOLOR 1
 set grc_wrap_commands cat cvs df diff dig gcc g++ ifconfig make mount mtr netstat pcommands cat cvs df diff dig gcc g++ ifconfig make mount mtr netstat ping ps tail traceroute wdiff
+
+set -U FZF_LEGACY_KEYBINDINGS 0
 
 # set SPACEFISH_PROMPT_ADD_NEWLINE false
 # set SPACEFISH_PROMPT_SEPARATE_LINE false
